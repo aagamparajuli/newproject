5722c98ec51168a61de4182607ab0420c102679c
aagam parajuli <aagamyt505@gmail.com>
2024-07-11 14:58:23 +0545

    new

--- Wandermate/src/App.jsx
+++ Wandermate/src/App.jsx
@@ -1,7 +1,27 @@
-export default function App() {
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Nav from "./Navigation";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./Landing";
+import CounterUse from "./demo";
+import HotelList from "./hotel";
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/demo" element={<CounterUse />} />
+            <Route path="/hotel" element={<HotelList />} />
+
+          </Routes>
+        </BrowserRouter>
+
+    </>
+  );
 }
+
+export default App;
